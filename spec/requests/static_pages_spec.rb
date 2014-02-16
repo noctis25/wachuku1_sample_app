3334c88999b9f619e477e86fd0d6e2011ea4ed8c
Ike Wachuku <ikechuku16@yahoo.com>
2014-02-16 18:26:08 -0500

    Added let function to tets

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,7 +1,8 @@
 require 'spec_helper'
 
 describe "Static pages" do
-let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
   describe "Home page" do
 
@@ -12,7 +13,7 @@ let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_title("#{base_title}  | Home")
+      expect(page).to have_title("#{base_title} | Home")
     end
   end
 
@@ -25,7 +26,7 @@ let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title("#{base_title}  | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
 
@@ -38,7 +39,7 @@ let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_title("#{base_title}  | About Us")
+      expect(page).to have_title("#{base_title} | About Us")
     end
   end
 
@@ -51,7 +52,7 @@ let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
     it "should have the title 'Contact'" do
       visit '/static_pages/contact'
-      expect(page).to have_title("#{base_title}  | Contact")
+      expect(page).to have_title("#{base_title} | Contact")
     end
   end
 end
