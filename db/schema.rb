f3e19f04ba8cd02e6ae43491523bd646ffff8621
Ike Wachuku <ikechuku16@yahoo.com>
2014-03-08 23:49:55 -0500

    Fixed a database column error

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140306054116) do
+ActiveRecord::Schema.define(version: 20140307051619) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 20140306054116) do
     t.string   "remember_token"
     t.boolean  "admin"
   end
-  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
 
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
 
 end
