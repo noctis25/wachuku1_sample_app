4387d8616bf827d995a39681d3844278edf5ba19
Ike Wachuku <ikechuku16@yahoo.com>
2014-03-08 22:26:34 -0500

    Finish user edit, update, index, and destroy actions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140303032742) do
+ActiveRecord::Schema.define(version: 20140306054116) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -20,6 +20,9 @@ ActiveRecord::Schema.define(version: 20140303032742) do
     t.datetime "updated_at"
     t.string   "password_digest"
     t.string   "remember_token"
+    t.boolean  "admin"
   end
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
+
 
 end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,41 +1,31 @@
 include ApplicationHelper
 
-def valid_signin(user)
-	fill_in "Email", 	with: user.email
-	fill_in "Password", with: user.password
-	click_button "Sign in"
-end
-
-def valid_signup
-	fill_in "Name",			with: "Example User"
-	fill_in "Email",		with: "user@example.com"
-	fill_in "Password",		with: "foobar"
-	fill_in "Confirmation",	with: "foobar"
-
+def sign_in(user)
+  visit signin_path
+  fill_in "Email",    with: user.email
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+  # Sign in when not using Capybara as well.
+  cookies[:remember_token] = user.remember_token
 end
 
 RSpec::Matchers.define :have_error_message do |message|
-	match do |page|
-		page.should have_selector('div.alert.alert-error', text: message)
-	end
+  match do |page|
+    expect(page).to have_selector('div.alert.alert-error', text: message)
+  end
 end
 
-RSpec::Matchers.define :have_success_message do |message|
-	match do |page|
-		page.should have_selector('div.alert.alert-success', text: message)
-	end
-end
-
-RSpec::Matchers.define :have_blank_signup_form_errors do
-	match do |page|
-		page.should have_content('error')
-       page.should have_content("The Password can't be blank")
-        page.should have_content("The Password is too short (minimum is 6 characters)")
-        page.should have_content("The Password confirmation can't be blank")
-	end
-        page.should have_content('The form contains 6 errors')
-        page.should have_content("The Name cannot be left blank")
-        page.should have_content("The Email cannot be left blank")
-        page.should have_content("The Email is invalid")
- 
+def sign_in(user, options={})
+  if options[:no_capybara]
+    # Sign in when not using Capybara.
+    remember_token = User.new_remember_token
+    cookies[:remember_token] = remember_token
+    user.update_attribute(:remember_token, User.hash(remember_token))
+     post sessions_path, {:session => { :email => user.email, :password => user.password }}
+  else
+    visit signin_path
+    fill_in "Email",    with: user.email
+    fill_in "Password", with: user.password
+    click_button "Sign in"
+  end
 end
