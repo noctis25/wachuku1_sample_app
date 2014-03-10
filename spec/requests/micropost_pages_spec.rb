98a39e696db67121564caffa156b77fc518d57c3
Ike Wachuku <ikechuku16@yahoo.com>
2014-03-09 21:21:01 -0400

    Finished ALL chapter 10 exercises

--- spec/requests/micropost_pages_spec.rb
+++ spec/requests/micropost_pages_spec.rb
@@ -29,8 +29,6 @@ describe "Micropost pages" do
         expect { click_button "Post" }.to change(Micropost, :count).by(1)
       end
     end
-  end
-
   describe "micropost destruction" do
     before { FactoryGirl.create(:micropost, user: user) }
 
@@ -42,4 +40,75 @@ describe "Micropost pages" do
       end
     end
   end
+
+   #exercise 10.5.1
+      describe "the sidebar micropost counts" do
+        describe "one posting" do
+          before do 
+            FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+            sign_in user
+            visit root_path
+          end
+
+          it { should have_content("1 micropost") }
+        end
+
+       describe "two postings" do
+          before do 
+            FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
+	    FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum 2")
+            sign_in user
+            visit root_path
+          end
+
+          it { should have_content("2 microposts") }
+        end
 end
+
+	#Exercise 10.5.2
+	describe "pagination" do
+         describe "more than one page" 
+	 before do 
+           31.times{ FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")}
+            sign_in user
+            visit root_path
+          end
+          it {should have_selector("div.pagination") }
+          it {should have_selector("li.prev.previous_page")}
+	end
+	describe "one page only" do
+	before do 
+           10.times{ FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")}
+            sign_in user
+            visit root_path
+          end
+          it {should_not have_selector("div.pagination") }
+          it {should_not have_selector("li.prev.previous_page")}
+	end
+	end
+
+	#exercise 10.5.4
+	describe "delete links" do
+	#the created user posts some microposts
+	before do 
+           10.times{ FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")}
+
+	#second user created
+	@attr = { :name => "Second User",
+          :email => "user@example.com",
+           :password => "foobar",
+           :password_confirmation => "foobar" }
+         @second_user = FactoryGirl.create(:user, @attr)     
+
+	#second user signs in and navigates to the first users profile
+	   sign_in @second_user
+           get root_path, :id => user
+	end
+	#no way to delete
+	it {should_not have_selector("a", :text => "delete")}
+	end
+
+end
+
+
+
